3d023d6e51ff40e821e6c94ee83b067c17bd5f04
manan999 <aggarwalmanan4@gmial.com>
2019-08-24 15:18:20 +0530

    added login blank file

---
+++ src/comps/Login/Register.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Register extends Component {
+	render() {
+		return (
+			<div>
+				This is Register
+			</div>
+		);
+	}
+}
+
+export default Register ;
