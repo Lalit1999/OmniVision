f759c5fc3ad84ff22bd066306b3569296305e58e
Lalit1999 <9lalitkumar9@gmail.com>
2019-08-24 13:45:58 +0530

    added slider

--- src/comps/home/Home.js
+++ src/comps/home/Home.js
@@ -2,7 +2,7 @@ import React from 'react' ;
 
 // import Banner from '../banner/Banner.js' ;
 // import BannerTwo from '../banner/BannerTwo.js' ;
-// import Slider from '../slider/Slider.js' ;
+import Slider from '../slider/Slider.js' ;
 // import Parallax from '../Parallax/Parallax.js' ;
 import './home.css' ;
 // import TestimonialSlider from '../slider/TestimonialSlider.js' ;
@@ -20,30 +20,30 @@ class Home extends React.Component
             {	style : {
 	            	backgroundImage: 'url(' + i4+ ')',
             	},
-	            title: 'PSYMENT',
+	            title: 'OmniVision',
 	            link : '/' ,
-	            message: 'Welcome To the World of Self Discovery',
+	            message: 'Magic Eye that understand images',
          	},
          	{	style : {
 	            	backgroundImage: 'url(' + i3+ ')',
             	},
-	            title: 'AQUESS',
-	            link : '/program/AQUESS' ,
-	            message: 'An Online Free Query System for Students',
+	            title: 'Face Detection',
+	            link : '/' ,
+	            message: 'Magic Eye detecting faces in your pictures',
          	},
             {	style : {
 	            	backgroundImage: 'url(' + i1+ ')',
             	},
-	            title: 'Sarathi',
-	            message: 'Workshop organising solution for Schools and Colleges' ,
-	            link : '/program/Sarathi'
+	            title: 'Color Detection',
+	            message: 'Magic Eye is not color blind' ,
+	            link : '/'
 	        } ,
 	        {	style : {
 	            	backgroundImage: 'url(' + i2+ ')',
             	},
-	            title: 'Personal Consultation',
-	            link : '/program/personal' ,
-	            message: 'Get personal consultation from our experts',
+	            title: 'General Model',
+	            link : '/' ,
+	            message: 'Day-to-Day items detected by Magic Eye',
          	},
         ];
 	}
@@ -52,7 +52,7 @@ class Home extends React.Component
 	{
 		return(
 			<div>
-				this is home
+				<Slider data={this.data} />
 			</div>
 		) ;
 	}
@@ -62,4 +62,3 @@ export default Home ;
 
 				
 				
-				// <Slider data={this.data} />
