e4be4c3092e19b8360a3bba3dcee8982e35d8050
manan999 <aggarwalmanan4@gmial.com>
2019-09-01 21:48:51 +0530

    logo added

--- src/comps/header2/Header2.js
+++ src/comps/header2/Header2.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import Eye from '../images/eye2.png' ;
 import './header2.css' ;
 
 class Header2 extends Component {
@@ -7,7 +8,12 @@ class Header2 extends Component {
 		return (
 			<div className="header2">
 				 <div className="header2-menu"> Menu </div>
-				 <div className="ov"> OmniVision </div>
+				 <div className="ov">
+				 	<p className="h2-para">
+				 		<img className="eye" alt="eye" src={Eye} />
+				 		<span className="ovname">OmniVision</span> 
+				 	</p> 
+				 </div>
 				 <div className="header2-login"> Login </div>
 			</div>
 		);
