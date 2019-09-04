5e4f2a9e6bdddcf0be1c5eac3e86d2df6b4818ef
Lalit1999 <9lalitkumar9@gmail.com>
2019-09-04 23:04:51 +0530

    made register page

--- src/comps/Login/Login.js
+++ src/comps/Login/Login.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom' ;
+import './Login.css' ;
 
-class Login extends Component {
+class Login extends Component
+{
+	constructor(props)
+	{
+		super(props) ;
+		this.state = {
+      		user : '' ,
+     		id : '' ,
+      		pw : '' ,
+		} ;
+	}
+	onIdChange = (event) => {
+    this.setState({ id: event.target.value}) ;
+  	} 
+  
+  	onPwChange = (event) => {
+    this.setState({ pw: event.target.value}) ;
+  	}
+
+  	checkUser = () => {
+    if( this.state.id === 'admin' && this.state.pw === 'Amazing#15')
+      this.setState({ user : this.state.id, pw: '', id: ''}) ;
+  	}
+
 	render() {
 		return (
-			<div>
-				This is Login
-			</div>
+			<div className = "bg">
+				<div className="login">
+	              <h3 className="login-title">Login </h3>
+	              <div>
+	                <label className="lbl">User : </label>
+	                <input  className="inp" type="text" onChange={this.onIdChange} name="name" value={this.state.id}/>
+	              </div>
+	              <div>
+	                <label className="lbl">Password : </label>
+	                <input className="inp" type="password" onChange={this.onPwChange} name="name" value={this.state.pw}/>
+	              </div>
+	              <div className="btn-con">
+	                <button onClick={this.checkUser}> Sign-in </button> 
+	                <Link className="btn-con2" to = '/register'>Register</Link> 
+	              </div>
+	            </div>
+			</div>          
 		);
 	}
 }
--- src/comps/Login/Register.js
+++ src/comps/Login/Register.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
+import './Register.css' ;
 
 class Register extends Component {
 	render() {
 		return (
-			<div>
-				This is Register
-			</div>
+			<div className = "bgk">
+				<div className="register">
+	              <h3 className="register-title">Register </h3>
+	              <div>
+	                <label className="lbel">User : </label>
+	                <input  className="inpu" type="text" name="name" />
+	              </div>
+	              <div>
+	                <label className="lbel">Password : </label>
+	                <input className="inpu" type="password"  name="name" />
+	              </div>
+	              <div>
+	                <label className="lbel">Re-enter Password : </label>
+	                <input className="inpu" type="password"  name="name" />
+	              </div>
+	              <div>
+	              	<label className="lbel">Mobile no. : </label>
+	                <input  className="inpu" type="text" name="name" />
+	              </div>
+	              <div>
+	              	<label className="lbel">Gmail-ID : </label>
+	                <input  className="inpu" type="text" name="name" />
+	              </div>
+	              <div className="btn-cona">
+	                <button > Submit </button> 
+	                
+	              </div>
+	            </div>
+			</div>     
 		);
 	}
 }
 
 export default Register ;
+
+
