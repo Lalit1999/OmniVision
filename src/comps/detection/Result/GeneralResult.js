d38553f5cf55fb1c9758e05e9d1c1d9b79adcd94
Lalit1999 <9lalitkumar9@gmail.com>
2019-10-15 22:48:08 +0530

    changes in g-result

--- src/comps/detection/Result/GeneralResult.js
+++ src/comps/detection/Result/GeneralResult.js
@@ -5,21 +5,19 @@ import './rescolor.css' ;
 
 class GeneralResult extends Component
 {
-	// createElements = () => {
-		//console.log(this.props.colors) ;
-		// return this.props.colors.map(color=> <Box key={(color.value+' ').slice(0, 5) + ' %'} 
-		// 	text1={color.value*100} text2={color.w3c.name} color={color.raw_hex}/>) ;
-	// } 
+	createElements = () => {
+		console.log(this.props.colors) ;
+		return this.props.colors.map(color=> <Box key={(color.value+' ').slice(0, 5) + ' %'} 
+			text1={color.value*100} text2={color.w3c.name} color={color.raw_hex}/>) ;
+	} 
 
 	render() {
 		return (
-			<div>this is GeneralResult.</div>
+			<div ref={this.props.r} className="box-con">
+				{ this.createElements() }
+			</div>
 		);
 	}
 }
 
 export default GeneralResult ;
-			
-			// <div ref={this.props.r} className="box-con">
-			// 	{ this.createElements() }
-			// </div>
