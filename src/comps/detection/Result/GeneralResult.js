76a438f1d56aea55da65e2ea7a977747a0b43b65
Lalit1999 <9lalitkumar9@gmail.com>
2019-10-18 15:23:14 +0530

    data display in general model

--- src/comps/detection/Result/GeneralResult.js
+++ src/comps/detection/Result/GeneralResult.js
@@ -6,11 +6,10 @@ import './rescolor.css' ;
 class GeneralResult extends Component
 {
 	createElements = () => {
-		// console.log(this.props.colors) ;
-		return this.props.colors.map(color=> <Box key={color.value} 
-			text1={((color.value*100)+' ').slice(0, 5) + ' %'} color={color.raw_hex}/>) ;
-			// text1={((color.value*100)+' ').slice(0, 5) + ' %'} text2={color.w3c.name} color={color.raw_hex}/>) ;
-			// https://uploads.themler.io/store/sample2-objects.png(test image link)
+		// console.log(this.props.concepts) ;
+		return this.props.concepts.map(concept=> <Box key={concept.name} 
+			text1={concept.name} middle="Probability :" text2={((concept.value*100)+' ').slice(0, 5)}  />) ;
+			// https://uploads.themler.io/store/sample2-objects.png(test image)
 	} 
 
 	render() {
