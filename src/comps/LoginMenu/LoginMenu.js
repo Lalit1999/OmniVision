9432b533146c10268adf82b2339f7139fd1e32bc
Lalit1999 <9lalitkumar9@gmail.com>
2020-03-21 13:58:22 +0530

    menu me thoda kam kara hai bas

--- src/comps/LoginMenu/LoginMenu.js
+++ src/comps/LoginMenu/LoginMenu.js
@@ -1,18 +1,31 @@
 import React from 'react' ;
+import {Link} from'react-router-dom' ;
 
 import './menu.css' ;
+import profile from "./profile.png" ;
+
 
 class LoginMenu extends React.Component
 {
+	constructor(props) {
+	    super(props)
+	    this.classMenu = {
+	    	face : 'item link' ,
+	    	color : 'item link' ,
+	    	general : 'item link' ,
+	    	age : 'item link' ,
+	    } ;
+	}
 	render()
 	{
 		return (
 			<div className="loginmenu">
+				<img src = {profile} alt="profile image" className="pi" />
 				<div className="burger" >
-					<a className="item link" href='/face'> Face Detection </a>
-					<a className="item link" href='/general'> Object Detection </a>
-					<a className="item link" href='/color'> Color Detection </a>
-					<a className="item link" href='/age'> Age & Gender Detection </a>
+					<Link className={this.classMenu.face} to='/face'> Face Detection </Link>
+					<Link className={this.classMenu.general} to='/general'> Object Detection </Link>
+					<Link className={this.classMenu.color} to='/color'> Color Detection </Link>
+					<Link className={this.classMenu.age} to='/age'> Age & Gender Detection </Link>
 				</div>
 			</div>
 			) ;
@@ -20,3 +33,4 @@ class LoginMenu extends React.Component
 }
 
 export default LoginMenu ;
+
