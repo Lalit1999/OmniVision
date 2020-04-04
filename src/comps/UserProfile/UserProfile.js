ac2314d05300195b21e11f43d555f2c965cf7058
Lalit1999 <9lalitkumar9@gmail.com>
2020-04-04 23:59:29 +0530

    changes in user profile

--- src/comps/UserProfile/UserProfile.js
+++ src/comps/UserProfile/UserProfile.js
@@ -3,12 +3,26 @@ import React from 'react' ;
 import './UserProfile.css' ;
 
 class UserProfile extends React.Component
-{
+{	
+	constructor(props) {
+	    super(props) ;
+	    this.state = {
+	    	userprofile : {} 
+	    } ;
+
+	}
+
+	
 	render()
 	{
 		return (
-			<div className="ptest">
-				This is UserProfile.
+			<div className="propfileBox">
+				<div className="ptest">
+					<p>Profile photo</p>
+					<p>Name</p>
+					<p>E-mail</p>
+					<p>Phone number</p>
+				</div>
 			</div>
 			) ;
 	}
