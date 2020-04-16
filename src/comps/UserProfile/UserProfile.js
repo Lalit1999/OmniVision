f13f50352a28c52278500ce7df96fe6a61caad09
Lalit1999 <9lalitkumar9@gmail.com>
2020-04-16 15:11:57 +0530

    added css of profile

--- src/comps/UserProfile/UserProfile.js
+++ src/comps/UserProfile/UserProfile.js
@@ -14,74 +14,46 @@ class UserProfile extends React.Component
 	
 	render()
 	{	console.log(this.props.user) ;
-		return (
-			<div className="propfileBox">
-				<div className="ptest">
-					<div className="left">
-						<div className = "left_corner_one">
-							<img src = {pencil} alt="pencil" className= "ui" />
+		if(this.props.user)
+		{
+			return (
+				<div className="propfileBox">
+					<div className="ptest">
+						<div className="left">
+							<div className = "left_corner_one">
+								<img src = {pencil} alt="pencil" className= "ui" />
+							</div>
+							<div className = "left_corner_two">
+								<p>Created at </p>
+	 							<p>{this.props.user.createdAt}</p>
+	 							<Link to="/history"> History </Link> 
+	 							<button className = "button button-a " >Logout</button>
+							</div>
 						</div>
-						<div className = "left_corner_two">
-							<p>Created at </p>
-							<p>{this.props.user.createdAt}</p>
-							<Link to="/history"> History </Link> 
-							<button className = "button button-a " >Logout</button>
-						</div>
-					</div>
-					<div className="right">
-						<div className = "right_corner_one">
-							<p>Name</p>
-							<p>{this.props.user.name}</p>							
-							<p>E-mail</p>
-							<p>{this.props.user.email}</p>
-							<p>Phone number</p>
-							<p>{this.props.user.mobile}</p>
-						</div>
-						<div className = "right_corner_two">
-							<button className = "button button-a " >Change password</button>
-							<button className = "button button-a " >Edit Data</button>
+						<div className="right">
+							<div className = "right_corner_one">
+								<p>Name</p>
+								<p>{this.props.user.name}</p>							
+								<p>E-mail</p>
+								<p>{this.props.user.email}</p>
+								<p>Phone number</p>
+								<p>{this.props.user.mobile}</p>
+							</div>
+							<div className = "right_corner_two">
+								<button className = "button button-a " >Change password</button>
+								<button className = "button button-a " >Edit Data</button>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		) ;
+			) ;
+		}
+		else
+		{	
+			return <Redirect to = '/login' />
+		}	
 		
 	}
 }
 
-export default UserProfile ;
-
-// if(this.props.user)
-// 		{
-// 			return (
-// 				<div className="propfileBox">
-// 					<div className="ptest">
-// 						<div className="left">
-// 							<div className = "left_corner_one">
-// 								<img src = {pencil} alt="pencil" className= "ui" />
-// 							</div>
-// 							<div className = "left_corner_two">
-// 								<p>Created at</p>
-// 								<Link to="/history"> History </Link> 
-// 								<p>Logout(button)</p>
-// 							</div>
-// 						</div>
-// 						<div className="right">
-// 							<div className = "right_corner_one">
-// 								<p>Name</p>
-// 								<p>E-mail</p>
-// 								<p>Phone number</p>
-// 							</div>
-// 							<div className = "right_corner_two">
-// 								<p>change password(link)</p>
-// 								<p>Edit(button)</p>
-// 							</div>
-// 						</div>
-// 					</div>
-// 				</div>
-// 			) ;
-// 		}
-// 		else
-// 		{	
-// 			return <Redirect to = '/login' />
-// 		}
+export default userprofile ;
